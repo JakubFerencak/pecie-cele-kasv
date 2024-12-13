9c1bde4116689bd7136125f55cb6487c8fda0260
Jakub Ferenčák <jakub.ferencak@kosickaakademia.sk>
2024-12-13 12:00:50 +0100

    zxz

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,8 +1,10 @@
+import { M } from '@angular/cdk/keycodes';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
